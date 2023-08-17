50b0058dedfee4ac28e35aeda5237eeec13b162f
Prakriti Pokharel <iprakritip@gmail.com>
2023-08-17 16:15:24 +0545

    feat : add all the quiz questions

--- quiz_game.py
+++ quiz_game.py
@@ -7,3 +7,41 @@ if playing.lower()!='Yes'.lower():
     quit();
 
 print("Well then, buckle up !!")
+
+answer = input("What's the full form of CPU? ")
+
+if answer.lower()=='central processing unit'.lower():
+    print('Correct!')
+else:
+    print("Oops!! Wrong answer.")
+
+answer = input("What does .py refer to in a python file? ")
+
+if answer.lower()=='python'.lower():
+    print('Correct!')
+else:
+    print("Oops!! Wrong answer.")
+
+
+answer = input("What is the capital of Turkey? ")
+
+if answer.lower()=='Ankara'.lower():
+    print('Correct!')
+else:
+    print("Oops!! Wrong answer.")
+
+
+answer = input("What is the full form of WHO? ")
+
+if answer.lower()=='world health organization'.lower():
+    print('Correct!')
+else:
+    print("Oops!! Wrong answer.")
+
+
+answer = input("How many bones do humans have? ")
+
+if int(answer)==206:
+    print('Correct!')
+else:
+    print("Oops!! Wrong answer.")
