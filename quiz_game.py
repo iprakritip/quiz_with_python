ca2dd9033586cd58962ad592e3148df9fce73f69
Prakriti Pokharel <iprakritip@gmail.com>
2023-08-17 16:05:16 +0545

    feat : ask first question

--- quiz_game.py
+++ quiz_game.py
@@ -1,1 +1,9 @@
 print("Welcome to the Quiz!!")
+
+playing = input("Are you ready to play? ")
+
+if playing.lower()!='Yes'.lower():
+    print("Sad to see you go :(")
+    quit();
+
+print("Well then, buckle up !!")
